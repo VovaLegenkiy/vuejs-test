ad461a94997b7482ce9e653be35fc2d9eb4640bb
Volodymer Lehenkyi <Volodymyr_Lehenkyi@epam.com>
2018-06-15 17:08:51 +0300

    starting a project

---
+++ src/components/About/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Hello from About</p>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'About'
+  }
+</script>
+
+<style scoped>
+
+</style>
