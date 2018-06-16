754fb469ce6a81144d87a1b2122d66bbcd6322df
VovaLegenkiy <vova.legenkiy@gmail.com>
2018-06-17 02:49:52 +0300

    added convert history component to observer converting history

---
+++ src/components/ConvertHistory/ConvertHistory.vue
@@ -0,0 +1,21 @@
+<template>
+<div v-show="history.length">
+  <p>Convert History</p>
+</div>
+</template>
+
+<script>
+  import {mapState} from 'vuex'
+  export default {
+    name: "ConvertHistory",
+    computed:{
+      ...mapState([
+        'history'
+      ])
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
